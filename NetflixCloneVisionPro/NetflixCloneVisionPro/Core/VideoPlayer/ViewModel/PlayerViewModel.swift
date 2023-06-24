88735983da0bb10db0d37ca10248d37906d94a55
Barış Özgen <barisozgen@hotmail.com>
2023-06-24 12:41:31 +0300

    Main Player View done

---
+++ NetflixCloneVisionPro/NetflixCloneVisionPro/Core/VideoPlayer/ViewModel/PlayerViewModel.swift
@@ -0,0 +1,131 @@
+//
+//  PlayerViewModel.swift
+//  NetflixCloneVisionPro
+//
+//  Created by Baris OZGEN on 24.06.2023.
+//
+
+import AVKit
+import SwiftUI
+import Observation
+
+@Observable class PlayerViewModel {
+    private let randomDemoVideos = [
+        URL(string: "https://v4.cdnpk.net/videvo_files/video/free/video0464/large_watermarked/_import_612333a5b77665.41253489_FPpreview.mp4")!,
+        URL(string: "https://v4.cdnpk.net/videvo_files/video/free/video0464/large_watermarked/_import_612725328cbc79.49168812_FPpreview.mp4")!,
+        URL(string: "https://v4.cdnpk.net/videvo_files/video/free/video0460/large_watermarked/_import_60d5777e472e47.79267516_FPpreview.mp4")!,
+        URL(string: "https://v4.cdnpk.net/videvo_files/video/free/video0466/large_watermarked/_import_614ebc62e143a4.87970316_FPpreview.mp4")!,
+        URL(string: "https://v4.cdnpk.net/videvo_files/video/free/video0467/large_watermarked/_import_615579a59b5687.27049363_FPpreview.mp4")!,
+        URL(string: "https://v4.cdnpk.net/videvo_files/video/free/video0471/large_watermarked/_import_618a60f421e389.11686272_FPpreview.mp4")!]
+    
+    private var timeObserverToken: Any? = nil
+    
+    let videoPlayer : AVPlayer
+    var videoDuration : String = "00:00:00"
+    var videoCurrentTime : Double = 0
+    var videoTimeLeft : Double = 1
+    var videoTotalSeconds : Double = 1
+    var videIsPlaying: Bool = true
+    var videoFrame : (width: CGFloat, height: CGFloat) = (1000,566)
+    var isFrameFullScreen : Bool = false
+    
+    init(){
+        videoPlayer = AVPlayer(url: Bundle.main.url(forResource: "netflix-intro-for-movie", withExtension: "mp4")!)
+        videoPlayer.actionAtItemEnd = .pause
+        // first show demo netflix video than select a random demo movie
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(playerItemDidReachEnd(notification:)),
+                                               name: .AVPlayerItemDidPlayToEndTime,
+                                               object: videoPlayer.currentItem)
+        videoPlayer.play()
+    }
+    
+    @objc func playerItemDidReachEnd(notification: Notification) {
+        removePeriodicTimeObserver()
+        videoPlayer.seek(to: CMTime.zero)
+        videoPlayer.replaceCurrentItem(with: AVPlayerItem(url: randomDemoVideos.randomElement()!))
+        videoPlayer.play()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.9) {
+            let totalSeconds = self.videoPlayer.currentItem?.duration.seconds ?? 0
+            self.videoCurrentTime = 0
+            self.videoTotalSeconds = totalSeconds
+            self.videoTimeLeft = totalSeconds
+            let (hh,mm,ss) = self.modifyVideoTime(totalSeconds)
+            self.videoDuration = "\(hh):\(mm):\(ss)"
+            self.addPeriodicTimeObserver()
+        }
+    }
+    
+    private func modifyVideoTime(_ seconds: Double) -> (String,String,String) {
+        let secondsInt = Int(seconds)
+        let (h,m,s) = (secondsInt / 3600, (secondsInt % 3600) / 60, (secondsInt % 3600) % 60)
+        let hh = h < 10 ? "0\(h)" : String(h)
+        let mm = m < 10 ? "0\(m)" : String(m)
+        let ss = s < 10 ? "0\(s)" : String(s)
+        return (hh,mm,ss)
+    }
+    
+    func addPeriodicTimeObserver() {
+        // Notify every second
+        let timeScale = CMTimeScale(NSEC_PER_SEC)
+        let time = CMTime(seconds: 0.29, preferredTimescale: timeScale)
+        
+        timeObserverToken = videoPlayer.addPeriodicTimeObserver(forInterval: time,
+                                                                queue: .main) {
+            [weak self] time in
+            // update player transport UI
+            self?.videoCurrentTime = time.seconds
+            self?.updateVideoDuration()
+            self?.setFrameSize()
+        }
+    }
+    
+    func removePeriodicTimeObserver() {
+        if let timeObserverToken = timeObserverToken {
+            videoPlayer.removeTimeObserver(timeObserverToken)
+            self.timeObserverToken = nil
+        }
+    }
+    
+    func updateVideoDuration(){
+        videoTimeLeft = videoTotalSeconds - videoCurrentTime
+        if videoTimeLeft > 0 {
+            let (hh,mm,ss) = modifyVideoTime(videoTimeLeft)
+            videoDuration = "\(hh):\(mm):\(ss)"
+        }
+    }
+    
+    func videoPlayStop(){
+        withAnimation(.spring()){
+            if videIsPlaying {
+                videoPlayer.pause()
+                videIsPlaying = false
+            }else {
+                videoPlayer.play()
+                videIsPlaying = true
+            }
+        }
+    }
+    
+    func videoChangeTime(isForward: Bool = true){
+        let nextSeconds = isForward ? videoCurrentTime+5 : videoCurrentTime-5
+        let timeCM = CMTime(seconds: nextSeconds, preferredTimescale: CMTimeScale(NSEC_PER_SEC))
+        videoPlayer.seek(to: timeCM)
+        
+    }
+    
+    func setVideoVolume(_ videoVolume:Double){
+        videoPlayer.volume = Float(videoVolume)
+    }
+    
+    func setFrameSize(){
+        /*let frameSize = videoPlayer.currentItem!.presentationSize
+         let fullScreenSize : (width: CGFloat, height: CGFloat) = ( UIScene.main!.frame.width, NSScreen.main!.frame.width)
+         
+         withAnimation(.spring()){
+         videoFrame = isFrameFullScreen ? fullScreenSize : (frameSize.width, frameSize.height)
+         }*/
+    }
+}
---
+++ NetflixCloneVisionPro/NetflixCloneVisionPro/Core/VideoPlayer/Views/MainPlayerView.swift
@@ -0,0 +1,147 @@
+//
+//  MainPlayerView.swift
+//  NetflixCloneVisionPro
+//
+//  Created by Baris OZGEN on 24.06.2023.
+//
+
+import SwiftUI
+
+struct MainPlayerView: View {
+    @Environment(\.dismiss) var dismiss
+    
+    private let viewModel = PlayerViewModel()
+    
+    @State private var isControlPanelVisible = false
+    @State private var isFullScreen = false
+    @State private var isVolumeHover = false
+    @State private var isDissmiss = false
+    
+    @State private var videoCurrentTime : Double = 0
+    @State private var videoVolume : Double = 1
+
+    var body: some View {
+        ZStack{
+            videoPlayerView
+            controlPanelView
+        }
+        .frame(minWidth: viewModel.videoFrame.width,
+               idealWidth: viewModel.videoFrame.width,
+               maxWidth: viewModel.videoFrame.width,
+               minHeight: viewModel.videoFrame.height,
+               idealHeight: viewModel.videoFrame.height,
+               maxHeight: viewModel.videoFrame.height)
+        .scaleEffect(isDissmiss ? 0 : 1)
+        .onTapGesture{withAnimation(.spring()){isControlPanelVisible.toggle()}}
+    }
+    private var videoPlayerView : some View {
+        PlayerViewRepresentable(videoPlayer: viewModel.videoPlayer)
+            .frame(maxWidth: .infinity)
+            .disabled(true)
+            .background(.purple)
+            .onAppear{
+                withAnimation(.spring()){isControlPanelVisible=true}
+            }
+            .onChange(of: viewModel.videoCurrentTime){
+                videoCurrentTime = viewModel.videoCurrentTime
+            }
+            /*.onReceive(NotificationCenter.default.publisher(for: NSApplication.didBecomeActiveNotification), perform: { _ in
+                NSApp.mainWindow?.standardWindowButton(.closeButton)?.isHidden = true
+                NSApp.mainWindow?.standardWindowButton(.miniaturizeButton)?.isHidden = true
+                NSApp.mainWindow?.standardWindowButton(.zoomButton)?.isHidden = true
+            })*/
+    }
+    private var controlPanelView: some View {
+        VStack(spacing:20){
+            HStack{
+                Image(systemName: "arrow.left")
+                    .scaledToFit()
+                    .frame(height: 24)
+                    .onTapGesture {
+                        viewModel.videoPlayStop()
+                        withAnimation(.spring()){isDissmiss.toggle()}
+                        withAnimation(.easeOut(duration: 3).delay(3)){dismiss()}
+                    }
+                Spacer()
+                Image(systemName: "flag")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 24)
+            }
+            .padding(.horizontal)
+            Spacer()
+            HStack{
+                Slider(value: $videoCurrentTime, in: 0...viewModel.videoTotalSeconds)
+                    .tint(.red)
+                    .foregroundColor(.red)
+                Text("\(viewModel.videoDuration)")
+                    .font(.headline)
+            }
+            .opacity(viewModel.videoCurrentTime < 1 || isVolumeHover ? 0.07 : 1)
+            HStack{
+                HStack(spacing:29){
+                    Image(systemName: viewModel.videIsPlaying ? "pause.fill" : "play.fill")
+                        .onTapGesture { viewModel.videoPlayStop() }
+                    Image(systemName: "gobackward.10")
+                        .onTapGesture { viewModel.videoChangeTime(isForward: false) }
+                    Image(systemName: "goforward.10")
+                        .onTapGesture { viewModel.videoChangeTime() }
+                    Image(systemName: videoVolume > 0.8  ? "speaker.wave.3.fill" :
+                            videoVolume > 0.5  ? "speaker.wave.2.fill" :
+                            videoVolume > 0.1  ? "speaker.wave.1.fill" : "speaker.slash.fill")
+                    .onTapGesture { withAnimation(.spring()){isVolumeHover.toggle()} }
+                    VStack{
+                        Slider(value: $videoVolume, in: 0...1)
+                            .tint(.red)
+                            .frame(width: 129, height: 40)
+                            .padding(4)
+                            .background(Color(.darkGray))
+                            .rotationEffect(.degrees(-90.0))
+                            .onChange(of: videoVolume) {oldVol,  newVol in
+                                withAnimation(.spring()){viewModel.setVideoVolume(newVol)} }
+                    }
+                    .padding(.leading, -114)
+                    .padding(.top, -129)
+                    .opacity(isVolumeHover ? 1 : 0)
+                }
+                Text("Content title: Episode title")
+                    .font(.title2)
+                    .frame(maxWidth: .infinity)
+                HStack(spacing:29){
+                    Image(systemName: "forward.end")
+                    Image(systemName: "rectangle.on.rectangle")
+                    Image(systemName: "text.bubble")
+                    Image(systemName: "stopwatch")
+                    Image(systemName: "rectangle.dashed")
+                        .onTapGesture {
+                            withAnimation(.spring()){
+                                viewModel.isFrameFullScreen.toggle()
+                            }
+                        }
+                }
+            }
+            .shadow(color: .black.opacity(0.29), radius: 3, x: 1, y: 1)
+            .fontWeight(.bold)
+            .padding(.bottom, 14)
+        }
+        .scaleEffect(y: isControlPanelVisible ? 1 : 1.29)
+        .opacity(isControlPanelVisible ? 1 : 0)
+        .foregroundColor(.white)
+        .padding()
+        .background(content: {
+            LinearGradient(colors: [.black.opacity(0.92),
+                                    .clear,
+                                    .clear,
+                                    .clear,
+                                    .clear,
+                                    .clear,
+                                    .black.opacity(0.92)], startPoint: .top, endPoint: .bottom)
+            .edgesIgnoringSafeArea(.all)
+        })
+    }
+}
+
+
+#Preview {
+    MainPlayerView()
+}
