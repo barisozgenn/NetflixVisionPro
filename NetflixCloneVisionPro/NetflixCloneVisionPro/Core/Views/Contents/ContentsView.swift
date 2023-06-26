1352fbf01ffe1bc02fd3f39a4a4637f89871bc8a
Barış Özgen <barisozgen@hotmail.com>
2023-06-26 13:34:24 +0300

    demo "browse by" view added

--- NetflixCloneVisionPro/NetflixCloneVisionPro/Core/Views/Contents/ContentsView.swift
+++ NetflixCloneVisionPro/NetflixCloneVisionPro/Core/Views/Contents/ContentsView.swift
@@ -12,7 +12,7 @@ struct ContentsView: View {
     @Binding var selectedContent: SelectedContent?
     @Binding var selectionListId: Int
     @Binding var selectedMenu: EMenu
-
+    
     @State private var scale = 0.0
     @State private var offsetY = 329.0
     @State private var offsetX = 329.0
@@ -20,9 +20,13 @@ struct ContentsView: View {
     
     var body: some View {
         ZStack{
-            headerView
+            if selectedMenu != .browseByLanguages {
+                headerView
+            }
             if selectedMenu == .movies {
                 movieContents
+            }else if selectedMenu == .browseByLanguages {
+                browseByContents
             }else {
                 homeContents
             }
@@ -72,8 +76,8 @@ struct ContentsView: View {
                     
                     ListTitleView(title: title, animationDelay: .constant(3.729 + Double(index) * 0.29))
                         .padding(.top, index == 0 ? 14.729 : 2.297)
-                    .offset(z: 1)
-                    .saturation(selectionListId == -1 ? 1 : 0.29)
+                        .offset(z: 1)
+                        .saturation(selectionListId == -1 ? 1 : 0.29)
                     
                     ContentListView(
                         selectedContent: $selectedContent,
@@ -95,8 +99,8 @@ struct ContentsView: View {
                     
                     ListTitleView(title: title, animationDelay: .constant(0.729 + Double(index) * 0.29))
                         .padding(.top, index == 0 ? 14.729 : 2.297)
-                    .offset(z: 1)
-                    .saturation(selectionListId == -1 ? 1 : 0.29)
+                        .offset(z: 1)
+                        .saturation(selectionListId == -1 ? 1 : 0.29)
                     
                     ContentListView(
                         selectedContent: $selectedContent,
@@ -110,6 +114,48 @@ struct ContentsView: View {
             }
         }
         .padding(.top, 329)
+    }
+    private var browseByContents: some View {
+        ScrollView(.vertical) {
+            VStack(alignment: .leading){
+                HStack{
+                    Text("Browse By Languages")
+                        .font(.title)
+                        .padding(.leading)
+                    Spacer()
+                    Picker("Select Your References", selection: .constant(0)) {
+                        Text("Original Language")
+                            .font(.headline)
+                    }
+                    .pickerStyle(.menu)
+                    Picker("", selection: .constant(0)) {
+                        Text("English")
+                            .font(.headline)
+                    }
+                    .pickerStyle(.menu)
+                    Picker("Sort By", selection: .constant(0)) {
+                        Text("Suggestions For You")
+                            .font(.headline)
+                    }
+                    .pickerStyle(.menu)
+                    .padding(.trailing)
+                }
+                .foregroundStyle(.white)
+                
+                ForEach(Array(contentTitles.shuffled().enumerated()), id: \.offset){ index, title in
+                    
+                    ContentListView(
+                        selectedContent: $selectedContent,
+                        selectionListId: $selectionListId,
+                        listId: index,
+                        animationDelay: 1.297 + Double(index) * 0.729,
+                        contents: Array(contents.shuffled()[0..<5])
+                    )
+                    .padding(.vertical, -75)
+                }
+            }
+        }
+        .padding(.top, 29)
     }
 }
 
